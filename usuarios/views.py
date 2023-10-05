31b59af09f25c41f79ebefee2871d34dd3ca8280
lucas-0liveira <lucas.oliver.70@hotmail.com>
2023-10-05 02:17:52 -0300

    PyStack

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
 
 def cadastro (request):
-    return HttpResponse('Olá Mundo!')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        primeiro_nome = request.POST.get('primeiro_nome')
+        ultimo_nome = request.POST.get('ultimo_nome')
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        email = request.POST.get('email')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'As senhas não são iguais')
+            return redirect('/usuarios/cadastro')
+        
+        if len(senha) < 6:
+            return redirect('/usuarios/cadastro')
+        
+        try:
+        #TODO: Validar se o username do usuário não existe
+            user = User.objects.create_user(
+                first_name = primeiro_nome,
+                last_name = ultimo_nome,
+                user = username,
+                email = email,
+                password = senha
+            )
+        except:
+            return redirect ('/usuarios/cadastro')
+        
+        return redirect('/usuarios/cadastro')
--- vitalab/settings.py
+++ vitalab/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'vitalab.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,9 +117,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATIC_ROOT = os.path.join('static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Messages
+
+from django.contrib.messages import constants
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.WARNING: 'alert-warning',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+}
